1cb38e83064bcf6e19fc3a28b2a51e8757d73740
kartikwar <kartiksirwani@gmail.com>
2020-05-05 15:29:39 +0530

    results added

--- scripts/create_labels.py
+++ scripts/create_labels.py
@@ -3,55 +3,6 @@ import numpy as np
 import  json
 import cv2
 
-def create_file(folder_path, type_file='train'):
-	text_lines = []
-	all_folders = os.listdir(folder_path)
-	folders_to_keep = []
-	val_folders = []
-	save_path = ''
-	all_jsons = []
-	if type_file == 'train':
-		folders_to_keep = [folder for folder in all_folders if folder not in val_folders]
-		save_path = '../train.txt'
-	else:
-		folders_to_keep = val_folders
-		save_path = '../val.txt'
-
-	json_matrix = [[os.path.join(folder_path, l_folder, l_file) for l_file in os.listdir(os.path.join(folder_path, l_folder)) if '.json' in l_file] for l_folder in folders_to_keep]
-	json_list = np.array(json_matrix).flatten().tolist()
-
-	for json_f in json_list:
-		root_path = json_f.replace(json_f.split("/")[-1], "")
-		im_path= ''
-
-		with open (json_f) as jf:
-			labels = json.load(jf)
-
-			for file_name, label in labels.items():
-
-				textline = ''
-				boxes = []
-				regions = label.get("regions")
-				im_path = os.path.join(root_path, label.get('filename'))
-
-				for i, region in enumerate(regions):
-					coordinates = region.get("shape_attributes")
-					x, y, width, height = coordinates['x'], coordinates['y'], coordinates['width'], coordinates[
-						'height']
-					boxes.extend([str(x) + ',' + str(y) + ',' + str(x + width) + ',' + str(y + height) + ',' + str(0)])
-
-				if len(boxes):
-					# textline += im_path + " " + str(boxes).replace("[", "").replace("]", "").strip()
-					textline += im_path + " " + " ".join(boxes).strip()
-					text_lines.append(textline)
-					temp = 0
-
-
-
-	f = open(save_path, 'w')
-	for ele in text_lines:
-		f.write(ele + '\n')
-	f.close()
 
 def create_file(folder_path, type_file='train'):
 	text_lines = []
@@ -131,10 +82,61 @@ def create_file(folder_path, type_file='train'):
 	temp = 0
 # labels_json = [l_file for l_file in os.listdir(l_folder) for l_folder in all_folders]
 
+def create_test_file(folder_path):
+
+	text_lines = []
+	test_save_path = '../test.txt'
+
+	images_list = [os.path.join(folder_path,f) for f in os.listdir(folder_path) if '.jpg' in f]
+
+
+	for im_path in images_list:
+		textline = ''
+		boxes = []
+		relative_im_path = im_path.replace('/home/kartik/Documents/aramco/darknet/', '')
+
+		boxes.extend([str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0)])
+
+		if len(boxes):
+			# textline += im_path + " " + str(boxes).replace("[", "").replace("]", "").strip()
+			textline += "\n".join(boxes).strip()
+			im_save_path = im_path.replace('.jpg', '.txt')
+			f = open(im_save_path, 'w')
+			f.write(textline)
+			f.close()
+			text_lines.append(relative_im_path)
+			# temp = 0
+
+
+
+	f = open(test_save_path, 'w')
+	for ele in text_lines:
+		f.write(ele + '\n')
+	f.close()
+
+def output_images(json_file, save_path):
+	with open(json_file) as jf:
+		coord_list = json.load(jf)
+		for coord_dict in coord_list:
+			rectangles = []
+			im_path = os.path.join('/home/kartik/Documents/aramco/darknet/', coord_dict['filename'])
+			im_save_path = os.path.join(save_path, ''.join(im_path.split('/')[-1]))
+			im = cv2.imread(im_path)
+			im_width, im_height = im.shape[1], im.shape[0]
+			for obj_dict in coord_dict['objects']:
+				relative_cords = obj_dict['relative_coordinates']
+				x_center, y_center, width, height = relative_cords['center_x'] * im_width, relative_cords['center_y'] * im_height, \
+													relative_cords['width'] * im_width, relative_cords['height'] * im_height
+				x0,y0, x1, y1 = x_center - (width/2), y_center - (height/2), x_center + (width/2), y_center + (height)/2
+				im = cv2.rectangle(im, (int(x0), int(y0)), (int(x1), int(y1)), (255,0,255), -1)
+			cv2.imwrite(im_save_path, im)
+	pass
 
 
 if __name__ == '__main__':
 	# all_folders = os.listdir("/home/kartik/Downloads/Data-20200416T060849Z-001/Data/completed")
-	create_file(folder_path="/home/kartik/Documents/aramco/darknet/build/darknet/x64/data/obj")
+	# create_file(folder_path="/home/kartik/Documents/aramco/darknet/build/darknet/x64/data/obj")
+	# create_test_file("/home/kartik/Documents/aramco/darknet/build/darknet/x64/data/test_obj")
+	output_images(json_file='/home/kartik/Documents/aramco/darknet/result.json',save_path='/home/kartik/Documents/aramco/darknet/results')
 	# create_file(folder_path="/home/kartik/Downloads/Data-20200416T060849Z-001/Data/completed", type_file='val')
 	pass
